797410a06380ba376c5c015edd46916608f5bd93
alexmiranda18 <alexsousamiranda@hotmail.com>
2024-09-19 16:41:57 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -17,6 +17,3 @@ app.post('/api/startup-idea', async (req, res) => {
   }
 });
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
